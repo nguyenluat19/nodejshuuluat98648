ebe01ffad67cbe06f4b998b981a9cc3cd6809394
nguyenluat19 <128703641+nguyenluat19@users.noreply.github.com>
2024-08-31 11:53:23 +0700

    Update http.js

--- initProject/http.js
+++ initProject/http.js
@@ -1,28 +1,11 @@
-const { createServer } = require("node:http");
-
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.end("<h1>hello</h1>");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-// const { exec } = require("child_process");
-
-// exec("npm init -y", (error, stdout, stderr) => {
-//   if (error) {
-//     console.error(`Lỗi khi chạy npm init: ${error.message}`);
-//     return;
-//   }
-
-//   if (stderr) {
-//     console.error(`Cảnh báo: ${stderr}`);
-//     return;
-//   }
-
-//   console.log(`Kết quả: ${stdout}`);
-// });
+const http = require("node:http");
+http
+  .createServer((request, response) => {
+    // response.write("<html>");
+    // response.write("<body>");
+    response.write("<h1>Hello +  World!</h1>");
+    // response.write("</body>");
+    // response.write("</html>");
+    response.end();
+  })
+  .listen(8080);
