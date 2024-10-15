176586720bc16cfc269786f066b2cd97dfa20dfe
nguyenluat19 <128703641+nguyenluat19@users.noreply.github.com>
2024-10-15 13:01:43 +0700

    Delete GIUA_KY_SPRINGBOOT/bookListManage directory

--- GIUA_KY_SPRINGBOOT/bookListManage/src/main/java/com/example/CategoryManager/Category/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CategoryManager.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
